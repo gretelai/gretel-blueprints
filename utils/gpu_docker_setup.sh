a44fd103439da37f11c6602ee401d2a641659371
John Myers <john@gretel.ai>
2021-06-02 08:26:01 -0400

    Docker NVIDIA setup script

---
+++ utils/gpu_docker_setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ $(lsb_release -rs) == "18.04" ]]; then
+
+    # NVIDIA Google Cloud VM Ubuntu 18.04
+    curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+    sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+    sudo apt install -y cuda
+
+else
+
+    echo "This must be run on Ubuntu 18.04"
+    exit 1
+
+fi
+
+# Install Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+
+# NVIDIA Docker tools
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get -y install nvidia-docker2
+sudo systemctl restart docker
+
+# Validate containers can see the GPUs
+sudo docker run --rm --gpus all nvidia/cuda:10.1-base nvidia-smi
