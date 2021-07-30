22d2fd8f853eae3da3ce75e0d9668e1d252876a1
John Myers <john@gretel.ai>
2021-07-30 12:03:19 -0400

    test update

--- test_config_templates.py
+++ test_config_templates.py
@@ -4,30 +4,41 @@ from pathlib import Path
 import pytest
 import requests
 from gretel_client import configure_session, ClientConfig, create_project
+from gretel_client.projects import Project
 import yaml
 
 
+_api_key = os.getenv("GRETEL_API_KEY")
+if not _api_key:
+    raise RuntimeError("API key env not set")
+
+
 configure_session(
-    ClientConfig(endpoint="https://api-dev.gretel.cloud")
+    ClientConfig(endpoint="https://api-dev.gretel.cloud", api_key=_api_key)
 )
 
 
 @pytest.fixture(scope="module")
-def project_name():
+def project(request) -> Project:
     proj = create_project()
-    yield proj.name
-    proj.delete()
+    yield proj
+
+    def delete_project():
+        proj.delete()
+
+    request.addfinalizer(delete_project)
 
 
 _configs = (Path(__file__).parent / "config_templates").glob("**/*.yml")
 
 
 @pytest.mark.parametrize("config_file", _configs)
-def test_configs(config_file, project_name):
+def test_configs(config_file, project: Project):
     _config_file = str(config_file)
     _config_dict = yaml.safe_load(open(_config_file).read())
+
     resp = requests.post(
-        f"https://api-dev.gretel.cloud/projects/{project_name}/models",
+        f"https://api-dev.gretel.cloud/projects/{project.name}/models",
         json=_config_dict,
         params={"dry_run": "yes"},
         headers={"Authorization": _api_key}
