405bcaeaad90498674f44a3f26e4203c36e4601a
Anastasia <anastasia.nesterenko.dev@gmail.com>
2022-07-18 20:52:26 -0400

    Using tmp_project

--- test_config_templates.py
+++ test_config_templates.py
@@ -3,10 +3,9 @@ from pathlib import Path
 
 import pytest
 import requests
-from gretel_client import configure_session, ClientConfig, create_project
-from gretel_client.projects import Project
 import yaml
-
+from gretel_client import ClientConfig, configure_session
+from gretel_client.projects import tmp_project
 
 _api_key = os.getenv("GRETEL_API_KEY")
 if not _api_key:
@@ -18,29 +17,19 @@ configure_session(
 )
 
 
-@pytest.fixture(scope="module")
-def project(request) -> Project:
-    proj = create_project()
-    yield proj
-
-    def delete_project():
-        proj.delete()
-
-    request.addfinalizer(delete_project)
-
-
 _configs = (Path(__file__).parent / "config_templates").glob("**/*.yml")
 
 
-@pytest.mark.parametrize("config_file", _configs)
-def test_configs(config_file, project: Project):
-    _config_file = str(config_file)
-    _config_dict = yaml.safe_load(open(_config_file).read())
-
-    resp = requests.post(
-        f"https://api-dev.gretel.cloud/projects/{project.name}/models",
-        json=_config_dict,
-        params={"dry_run": "yes"},
-        headers={"Authorization": _api_key}
-    )
-    assert resp.status_code == 200
+@pytest.mark.parametrize(
+    "_config_file", ["/".join(str(_config).split("/")[-4:]) for _config in _configs]
+)
+def test_configs(_config_file):
+    with tmp_project() as project:
+        _config_dict = yaml.safe_load(open(_config_file).read())
+        resp = requests.post(
+            f"https://api-dev.gretel.cloud/projects/{project.name}/models",
+            json=_config_dict,
+            params={"dry_run": "yes"},
+            headers={"Authorization": _api_key},
+        )
+        assert resp.status_code == 200
