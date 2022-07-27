932ada790f969e0f4aad8d5a249453997f05c691
Anastasia <anastasia@gretel.ai>
2022-07-27 13:29:19 -0400

    PR to main (#109)
    * Update CTGAN config based on hyperparameter sweep
    
    * Update low record count config with new sweep
    
    * Fix name in config
    
    * Fix filter value
    
    * Config files check on pr (#98)
    
    * Remove verbose option from CTGAN configs (#107)
    
    Co-authored-by: Andrew Carr <andrew@gretel.ai>
    Co-authored-by: Andrew Carr <andrewnc@users.noreply.github.com>

--- test_config_templates.py
+++ test_config_templates.py
@@ -3,44 +3,39 @@ from pathlib import Path
 
 import pytest
 import requests
-from gretel_client import configure_session, ClientConfig, create_project
-from gretel_client.projects import Project
 import yaml
-
+from gretel_client import ClientConfig, configure_session
+from gretel_client.projects import Project, tmp_project
 
 _api_key = os.getenv("GRETEL_API_KEY")
 if not _api_key:
     raise RuntimeError("API key env not set")
 
+_cloud_url = os.getenv("GRETEL_CLOUD_URL")
+if not _cloud_url:
+    raise RuntimeError("Gretel cloud url env not set")
 
-configure_session(
-    ClientConfig(endpoint="https://api-dev.gretel.cloud", api_key=_api_key)
-)
+configure_session(ClientConfig(endpoint=_cloud_url, api_key=_api_key))
 
 
-@pytest.fixture(scope="module")
-def project(request) -> Project:
-    proj = create_project()
-    yield proj
-
-    def delete_project():
-        proj.delete()
-
-    request.addfinalizer(delete_project)
+@pytest.fixture(scope="session")
+def project() -> Project:
+    with tmp_project() as project:
+        yield project
 
 
 _configs = (Path(__file__).parent / "config_templates").glob("**/*.yml")
 
 
-@pytest.mark.parametrize("config_file", _configs)
-def test_configs(config_file, project: Project):
-    _config_file = str(config_file)
+@pytest.mark.parametrize(
+    "_config_file", ["/".join(str(_config).split("/")[-4:]) for _config in _configs]
+)
+def test_configs(_config_file, project: Project):
     _config_dict = yaml.safe_load(open(_config_file).read())
-
     resp = requests.post(
-        f"https://api-dev.gretel.cloud/projects/{project.name}/models",
+        f"{_cloud_url}/projects/{project.name}/models",
         json=_config_dict,
         params={"dry_run": "yes"},
-        headers={"Authorization": _api_key}
+        headers={"Authorization": _api_key},
     )
     assert resp.status_code == 200
