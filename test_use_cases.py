682cdd2281c4dd242ae951e80687909ba902055b
Laura <laura@gretel.ai>
2022-09-27 10:20:58 -0600

    [FEAT] INT-565: Add initial UseCase support (#135)
    ## Problem
    We want to provide new "use cases" in Console to help folks find the best workflow for their jobs. We want to be able to make quick changes to the available use cases, including associated thumbnail images, without needing to redeploy Console.
    
    ## Solution
    Define the use case card configs here in gretel-blueprints repo, which we can then query from the Console. This also adds the initial set of images we plan to use. I added a test suite around the use cases, including using JSON Schema to validate that each card conforms to a data shape we are expecting (e.g., required fields, enum values). In a followup PR I plan to add a GitHub Action to run these automatically in CI. _Misc: updated repo README to include new directory info._
    
    ## Notes
    Initial use cases / text is likely to change still, these are placeholder values that will let me keep developing and wire everything together. The great thing is that the way we're designing this makes it easy to update these!

---
+++ test_use_cases.py
@@ -0,0 +1,103 @@
+import json
+from pathlib import Path
+
+import jsonschema
+from jsonschema import validate
+
+card_schema = {
+    "type": "object",
+    "properties": {
+        "title": {"type": "string"},
+        "description": {"type": "string"},
+        "imageName": {"type": "string"},
+        "model": {
+            "type": "string",
+            "enum": [
+                "synthetics",
+                "transform",
+                "classify",
+                "ctgan",
+                "amplify",
+                "gpt_x",
+                "evaluate",
+            ],
+        },
+        "defaultConfig": {"type": "string"},
+        "sampleDataset": {
+            "type": "object",
+            "properties": {
+                "fileName": {"type": "string"},
+                "description": {"type": "string"},
+                "records": {"type": "number"},
+                "fields": {"type": "number"},
+                "trainingTime": {"type": "string"},
+            },
+            "required": [
+                "fileName",
+                "description",
+                "records",
+                "fields",
+                "trainingTime",
+            ],
+        },
+        "docsUrl": {"type": "string"},
+        "tag": {
+            "type": "string",
+            "enum": ["New", "Beta", "Preview", "Popular", "Deprecated"],
+        },
+        "gtmId": {"type": "string"},
+    },
+    "required": ["title", "description", "gtmId", "imageName"],
+}
+
+
+def test_use_cases():
+    usecases_JSON = Path(__file__).parent / "use_cases/gretel.json"
+    is_valid = validate_JSON(usecases_JSON.open())
+    assert is_valid
+
+    titles = []
+    gtm_ids = []
+
+    cards_data = json.load(usecases_JSON.open())
+    for card in cards_data["cards"]:
+        card_is_valid = validate_use_case_JSON(card)
+        assert card_is_valid
+        gtm_ids.append(card["gtmId"])
+        titles.append(card["title"])
+        validate_images_exist(card["imageName"])
+
+    validate_unique(gtm_ids)
+    validate_unique(titles)
+
+
+def validate_JSON(json_data):
+    try:
+        json.load(json_data)
+    except ValueError as err:
+        return False
+    return True
+
+
+def validate_use_case_JSON(card):
+    try:
+        validate(instance=card, schema=card_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print("error format:", err)
+        return False
+    return True
+
+
+def validate_unique(field_values):
+    assert len(field_values) == len(set(field_values))
+
+
+def validate_images_exist(image_name):
+    dir_path = "use_cases/images"
+    split_name = image_name.split(".")
+    two_x = split_name[0] + "@2x." + split_name[1]
+    three_x = split_name[0] + "@3x." + split_name[1]
+
+    assert (Path(__file__).parent / dir_path / image_name).is_file()
+    assert (Path(__file__).parent / dir_path / two_x).is_file()
+    assert (Path(__file__).parent / dir_path / three_x).is_file()
