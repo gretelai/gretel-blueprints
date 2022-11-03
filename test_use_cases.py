cc7e5b9fa092e2a76ec27c26ad2f54dfe70313a5
Laura Steadman <laura@gretel.ai>
2022-11-03 15:29:18 -0600

    add new value to test so it passes

--- test_use_cases.py
+++ test_use_cases.py
@@ -13,15 +13,16 @@ card_schema = {
         "modelType": {
             "type": "string",
             "enum": [
-                "synthetics",
-                "transform",
-                "classify",
-                "ctgan",
                 "actgan",
                 "amplify",
-                "gpt_x",
+                "classify",
+                "ctgan",
                 "evaluate",
+                "gpt_x",
                 "lstm",
+                "synthetics",
+                "timeseries_dgan",
+                "transform",
             ],
         },
         "modelCategory": {
