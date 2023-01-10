6482f0a4bfee6ff69ddf66548a721b25076c0e00
John T. Myers <9696606+johntmyers@users.noreply.github.com>
2023-01-10 11:50:55 -0500

    stage use case card resources (#190)

---
+++ test_use_cases.py
@@ -0,0 +1,139 @@
+import json
+from pathlib import Path
+
+import jsonschema
+from jsonschema import validate
+
+card_schema = {
+    "type": "object",
+    "properties": {
+        "gtmId": {"type": "string"},
+        "title": {"type": "string"},
+        "description": {"type": "string"},
+        "cardType": {"type": "string", "enum": ["Console", "Notebook"]},
+        "imageName": {"type": "string"},
+        "tag": {
+            "type": "string",
+            "enum": ["New", "Beta", "Preview", "Popular", "Deprecated"],
+        },
+        "modelType": {
+            "type": "string",
+            "enum": [
+                "actgan",
+                "amplify",
+                "classify",
+                "ctgan",
+                "evaluate",
+                "gpt_x",
+                "lstm",
+                "synthetics",
+                "timeseries_dgan",
+                "transform",
+            ],
+        },
+        "modelCategory": {
+            "type": "string",
+            "enum": [
+                "synthetics",
+                "transform",
+                "classify",
+                "evaluate",
+            ],
+        },
+        "defaultConfig": {"type": "string"},
+        "sampleDataset": {
+            "type": "object",
+            "properties": {
+                "fileName": {"type": "string"},
+                "description": {"type": "string"},
+                "records": {"type": "number"},
+                "fields": {"type": "number"},
+                "trainingTime": {"type": "string"},
+                "bytes": {"type": "number"},
+            },
+            "required": [
+                "fileName",
+                "description",
+                "records",
+                "fields",
+                "trainingTime",
+                "bytes",
+            ],
+        },
+        "detailsFileName": {"type": "string"},
+        "button1": {
+            "type": "object",
+            "properties": {"label": {"type": "string"}, "link": {"type": "string"}},
+        },
+        "button2": {
+            "type": "object",
+            "properties": {"label": {"type": "string"}, "link": {"type": "string"}},
+        },
+    },
+    "required": ["gtmId", "title", "description", "cardType", "imageName"],
+}
+
+
+def test_use_cases():
+    usecases_JSON = Path(__file__).parent / "use_cases/gretel.json"
+    is_valid = validate_JSON(usecases_JSON.open())
+    assert is_valid
+
+    titles = []
+    gtm_ids = []
+
+    cards_data = json.load(usecases_JSON.open())
+    for card in cards_data["cards"]:
+        card_is_valid = validate_use_case_JSON(card)
+        assert card_is_valid
+        gtm_ids.append(card.get("gtmId"))
+        titles.append(card.get("title"))
+        validate_images_exist(card.get("imageName"))
+        validate_config_file_exists(card.get("defaultConfig"))
+        validate_details_file_exists(card.get("detailsFileName"))
+
+    validate_unique(gtm_ids)
+    validate_unique(titles)
+
+
+def validate_JSON(json_data):
+    try:
+        json.load(json_data)
+    except ValueError as err:
+        return False
+    return True
+
+
+def validate_use_case_JSON(card):
+    try:
+        validate(instance=card, schema=card_schema)
+    except jsonschema.exceptions.ValidationError as err:
+        print("error format:", err)
+        return False
+    return True
+
+
+def validate_unique(field_values):
+    assert len(field_values) == len(set(field_values))
+
+
+def validate_images_exist(image_name):
+    dir_path = "use_cases/images"
+    split_name = image_name.split(".")
+    two_x = split_name[0] + "@2x." + split_name[1]
+    three_x = split_name[0] + "@3x." + split_name[1]
+
+    assert (Path(__file__).parent / dir_path / image_name).is_file()
+    assert (Path(__file__).parent / dir_path / two_x).is_file()
+    assert (Path(__file__).parent / dir_path / three_x).is_file()
+
+
+def validate_config_file_exists(config_path):
+    if config_path:
+        assert (Path(__file__).parent / config_path).is_file()
+
+
+def validate_details_file_exists(fileName):
+    dir_path = "use_cases/details"
+    if fileName:
+        assert (Path(__file__).parent / dir_path / fileName).is_file()
---
+++ use_cases/details/evaluate-downstream-ml-accuracy.md
@@ -0,0 +1,9 @@
+Customers frequently ask whether synthetic data is of high enough quality to train downstream machine learning tasks. Classifiers, for example, require highly accurate data before they can be usefully deployed.
+
+The term "downstream" has multiple connotations. It could refer to a step in the system that happens after data has been processed and transformed. It can also refer to the machine learning step itself. In this instance, we refer to the downstream machine learning classifier.
+
+### Notebook Details
+
+This notebook uses Gretel's ACTGAN model to generate synthetic data from a highly dimensional dataset and then verifies its quality when training a downstream classifier using the open source AutoML PyCaret library.
+
+[Read our blog post](https://gretel.ai/blog/downstream-ml-classification-with-gretel-actgan-and-pycaret) or run the notebook to try it out yourself.
