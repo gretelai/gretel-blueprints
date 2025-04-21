69f47f51b264d1b94049fe1895c6489ae7c2dd4e
Matt Kornfield <mckornfield@gmail.com>
2025-04-21 14:23:30 -0700

    add collab links pointing to main and 101 notebook (#587)

---
+++ utils/add-collab-link.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "nbformat",
+# ]
+# ///
+import argparse
+
+import nbformat
+
+
+def main(file_path: str):
+    with open(file_path, encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+        # _parent means same window, different tab
+        # Note, this is to "develop" for the develop branch
+        collab_link = f"""<a target="_parent" href="https://colab.research.google.com/github/gretelai/gretel-blueprints/blob/main/{file_path}">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>"""
+        for cell in notebook["cells"]:
+            if (
+                'href="https://colab.research.google.com/github/gretelai'
+                in cell["source"]
+            ):
+                cell["source"] = collab_link
+                break
+        else:
+            collab_cell = nbformat.v4.new_markdown_cell(collab_link)
+            notebook["cells"].insert(0, collab_cell)
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        nbformat.write(notebook, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path")
+    args = parser.parse_args()
+    main(args.file_path)
